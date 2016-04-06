24293fa6a01c67d39bfdfb5a8703567475d607d4
Matthew Krauss <m.d.krauss@gmail.com>
2016-04-05 22:57:00 -0400

    Miscellaneous

--- tic/Tic.php
+++ tic/Tic.php
@@ -7,38 +7,88 @@ use Functional as F;
 class Tic {
     public function __construct($argv) {
         /*
+         * Initialize application
+         */
+
+        $this->load_parameters($argv);
+
+        $this->config = $this->load_config(
+            F\pick($this->params, 'config file', 'tic.conf'));
+
+        var_export(['called_as' => $this->called_as,
+                    'command' => $this->command,
+                    'params' => $this->params,
+                    'args' => $this->args]);
+
+        echo PHP_EOL;
+        $this->connect_db();}
+
+
+    private function load_parameters($argv) {
+        /*
          * Parse options
          */
-        $this->called_as = array_shift($argv);
-        $this->command = $argv[0];
 
-        list($errors, $params, $args) = getopts(
-            ['f' => ['Vs', 'f', 'file'],
-             'g' => ['Vs', 'g']],
+        $this->called_as = array_shift($argv);
+        $this->command = array_shift($argv);
+
+        list($errors, $this->params, $this->args) = getopts(
+            ['config file' => ['Vs', 'c', 'conf']],
             $argv);
 
         if ($errors) {
             // Handle errors?
-            var_dump($errors);
+            var_dump($errors); echo PHP_EOL;
             exit;}}
+
+
+    private function load_config($file) {
+        /*
+         * 
+         */
+        
+    }
 
 
     public function run() {
         /*
-         * Main function
+         * Main entry point - switches control based on command
          */
 
         switch ($this->command) {
-            case 'deploy': $this->run_deploy();
-            case 'revert': $this->run_revert();
-            case 'redeploy': $this->run_redeploy();
-            default: echo "Bad command!\n"; exit;}}
+            case 'deploy': $this->run_deploy(); break;
+            case 'revert': $this->run_revert(); break;
+            case 'redeploy': $this->run_redeploy(); break;
+            default: throw new \tic\exception\BadCommandException(
+                'Must give valid command');}}
 
-    private function deploy() {
-        
-    }
+
+    private function run_deploy() {
+        /*
+         * Deploy a set of changes - a target change and all
+         * dependencies of it, or all un-deployed changes.
+         */
+
+        $this->db()->transaction(function($db) {
+            $this->deploy_changes(
+                empty($this->args)
+                ? $this->plan
+                : $this->plan->subplan(
+                    $this->deployed_changes(),
+                    array_shift($this->args)));});}
+
+
+    private function load_plan($plan_dir) {
+        /*
+         * Loads a plan of changes from the given $plan_dir
+         */
+        $this->plan = new Plan ($plan_dir);}
+
 
     private $called_as;
     private $command;
     private $options;
+    private $params;
+    private $args;
+    private $config;
 }
