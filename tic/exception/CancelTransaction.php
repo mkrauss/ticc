f97308b7081d729d7cb3e8b0e115af23611d1c25
Matthew Krauss <m.d.krauss@gmail.com>
2016-04-13 23:57:04 -0400

    CancelTransaction exception

---
+++ tic/exception/CancelTransaction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace tic\exception;
+
+/*
+ *This is an atypical exception intended to be thrown and caught
+ *within Database::exec_to_rollback (or elsewhere if needed) to
+ *indicate that everything went fine but to break the transaction and
+ *cause a rollback.
+ */
+
+class CancelTransaction extends \Exception{};
