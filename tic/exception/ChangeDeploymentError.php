f0ec83866afe2d58e29516e2d8315fbb6b3840a3
Matthew Krauss <m.d.krauss@gmail.com>
2016-04-19 22:33:47 -0400

    Give reasons to ChangeDeploymentError

--- tic/exception/ChangeDeploymentError.php
+++ tic/exception/ChangeDeploymentError.php
@@ -2,4 +2,15 @@
 
 namespace tic\exception;
 
-class ChangeDeploymentError extends \Exception{};
+class ChangeDeploymentError extends \Exception{
+    public function add_reason($reason) {
+        /*
+         * Attach a reason description
+         */
+        $this->reason = $reason;}
+
+    public function __toString() {
+        return __CLASS__ . ": {$this->message} ({$this->reason})\n";}
+
+    private $reason;
+};
