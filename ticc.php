4bd73a425a819655f8e8c39cca5e6a5eea417998
Matthew Krauss <m.d.krauss@gmail.com>
2016-08-29 22:12:54 -0400

    feat: error exceptions
    can't believe I didn't do that sooner :/

--- ticc.php
+++ ticc.php
@@ -25,6 +25,15 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/alexkazik/getopts/getopts.php';
 
+function exception_error_handler($severity, $message, $file, $line) {
+    if (!(error_reporting() & $severity)) {
+        // This error code is not included in error_reporting
+        return;
+    }
+    throw new ErrorException($message, 0, $severity, $file, $line);
+}
+set_error_handler("exception_error_handler");
+
 try {
     echo <<<EOT
  _____ ___ ___ ___
